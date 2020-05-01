792b1357a98948f9b394a953bb1f67933f8369d3
Rúben Silva <ruben.silva@grama.io>
2020-05-01 19:28:53 +0100

    Homepage v2 layout completed

---
+++ assets/js/swipper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
